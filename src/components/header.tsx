809290a372ab0067aa0c580a726cd908c0097550
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-12 22:59:56 +0200

    feat: shadcn@latest init

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,39 @@
 import { Link } from 'waku';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
 
 export const Header = () => {
   return (
     <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0">
-      <h2 className="text-lg font-bold tracking-tight">
-        <Link to="/">Waku starter</Link>
-      </h2>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link to="/">
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <span>Home</span>
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/about">
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <span>About</span>
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </header>
   );
 };
