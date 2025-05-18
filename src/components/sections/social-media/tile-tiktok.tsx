30d694e55a258843979c477427e24e76d3ef0c39
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-18 20:49:24 +0200

    feat: improve tiles

--- src/components/sections/social-media/tile-tiktok.tsx
+++ src/components/sections/social-media/tile-tiktok.tsx
@@ -1,7 +1,8 @@
-import IconTikTok from '@/assets/svg/social-media/tiktok/logo--colorful.svg?react';
+import type { ComponentProps } from 'react';
 import { cn } from '@/lib/utils';
+import IconTikTok from '@/assets/svg/social-media/tiktok/logo--colorful.svg?react';
 
-export const TileTikTok = ({ className }: React.ComponentProps<'div'>) => {
+export const TileTikTok = ({ className }: ComponentProps<'div'>) => {
   return (
     <div className={cn('grid rounded-md bg-black group', className)}>
       <div className="place-self-center transition-[width,height,scale] duration-420 size-20 group-hover:scale-120">
