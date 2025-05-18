b92aa7c0871c64d3a59ea303144a6d1b2a733767
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-19 01:00:46 +0200

    refactor: improve daria tile

--- src/components/sections/social-media/tile-daria.tsx
+++ src/components/sections/social-media/tile-daria.tsx
@@ -3,10 +3,16 @@ import { cn } from '@/lib/utils';
 
 export const TileDaria = ({ className }: ComponentProps<'div'>) => {
   return (
-    <div className={cn('relative', className)}>
+    <div className={cn('group relative rounded-md overflow-clip', className)}>
       <img
         src="/images/daria-and-the-popcorn.jpg"
-        className="rounded-md h-full object-cover object-left"
+        className="h-full object-cover object-left"
+      />
+      <img
+        src="/images/daria-and-the-popcorn--hover.jpg"
+        loading="lazy"
+        fetchPriority="low"
+        className="opacity-0 group-hover:pointer-fine:opacity-100 transition-[opacity] absolute top-0 w-full h-full object-cover object-left"
       />
     </div>
   );
