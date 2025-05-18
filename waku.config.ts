d8adee7b513cef7fb97f90e472d06172d7e9b913
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-18 10:40:24 +0200

    chore: install vite-plugin-svgr

--- waku.config.ts
+++ waku.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'waku/config';
 import { fileURLToPath } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   unstable_viteConfigs: {
     common: () => ({
-      plugins: [tailwindcss()],
+      plugins: [tailwindcss(), svgr()],
       resolve: {
         alias: {
           '@': fileURLToPath(new URL('./src', import.meta.url)),
