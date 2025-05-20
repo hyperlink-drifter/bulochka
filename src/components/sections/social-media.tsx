6acd532e70aaca6914d23b944e2308a3f207f40b
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-21 01:55:24 +0200

    feat: add some high quality text

--- src/components/sections/social-media.tsx
+++ src/components/sections/social-media.tsx
@@ -5,10 +5,24 @@ import { TileVideo } from '@/components/tiles/tile-video';
 import { TileDaria } from '@/components/tiles/tile-daria';
 import { TileNewsletter } from '@/components/tiles/tile-newsletter';
 import { Section } from '@/components/layouts/section';
+import H from '@/components/layouts/headings';
+import A from '@/components/layouts/links';
 
 export const SocialMedia = () => {
   return (
     <Section>
+      <H as="h2">Stay in touch, give us a follow</H>
+      <p>You need content? We have content content content</p>
+      <p>
+        Check out our <A href="https://telegram.org/">Telegram</A>,{' '}
+        <A href="https://www.instagram.com/watch.with.daria/">Instagram</A> and{' '}
+        <A href="https://www.tiktok.com/@watch.with.ddaria">TikTok</A> to stay
+        up to date.
+      </p>
+      <p>
+        You need more? We got you covered: Subscribe to our{' '}
+        <A to="/">Newsletter</A> for the latest tasty treats!
+      </p>
       <div className="grid grid-cols-2 md:grid-cols-4 grid-flow-row-dense gap-3 sm:gap-4">
         <TileVideo className="col-span-1 row-span-2" />
         <TileTelegram className="col-span-1 row-span-1 aspect-square" />
