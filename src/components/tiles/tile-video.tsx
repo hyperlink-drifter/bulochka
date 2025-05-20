a1a5084234d91b962879cd3333710b0e533122d5
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-21 01:19:19 +0200

    feat: add playsinline

--- src/components/tiles/tile-video.tsx
+++ src/components/tiles/tile-video.tsx
@@ -5,7 +5,13 @@ import { Tile } from '@/components/tiles/tile-base';
 export const TileVideo = ({ className }: ComponentProps<'div'>) => {
   return (
     <Tile className={cn('', className)}>
-      <video autoPlay muted loop className="w-full h-full object-cover">
+      <video
+        playsInline
+        autoPlay
+        muted
+        loop
+        className="w-full h-full object-cover"
+      >
         <source src="/videos/daria-92.mp4" type="video/mp4" />
       </video>
     </Tile>
