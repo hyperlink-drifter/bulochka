a8afd74c77a32becf19820ac70ccec3fce6d7a8c
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-21 14:20:56 +0200

    refactor: remove overscroll-none

--- src/pages/_root.tsx
+++ src/pages/_root.tsx
@@ -4,7 +4,7 @@ type RootProps = { children: ReactNode };
 
 export default async function RootElement({ children }: RootProps) {
   return (
-    <html lang="en" className="overscroll-none overflow-x-clip bg-black">
+    <html lang="en" className="overflow-x-clip bg-black">
       <head></head>
       <body data-version="1.0" className="">
         {children}
