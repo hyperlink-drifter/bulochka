b1be774f5b96f1ec251a0d909d4c6219cb18e9ce
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-22 00:18:43 +0200

    refactor: stay true to bento

--- src/components/sections/social-media.tsx
+++ src/components/sections/social-media.tsx
@@ -7,29 +7,41 @@ import { TileNewsletter } from '@/components/tiles/tile-newsletter';
 import { Section } from '@/components/layouts/section';
 import H from '@/components/layouts/headings';
 import A from '@/components/layouts/links';
+import { Tile } from '@/components/tiles/tile-base';
 
 export const SocialMedia = () => {
   return (
     <Section>
-      <H as="h2">Stay in touch, give us a follow</H>
-      <p>You need content? We have content content content</p>
-      <p>
-        Check out our <A href="https://telegram.org/">Telegram</A>,{' '}
-        <A href="https://www.instagram.com/watch.with.daria/">Instagram</A> and{' '}
-        <A href="https://www.tiktok.com/@watch.with.ddaria">TikTok</A> to stay
-        up to date.
-      </p>
-      <p>
-        You need more? We got you covered: Subscribe to our{' '}
-        <A to="/">Newsletter</A> for the latest tasty treats!
-      </p>
-      <div className="grid grid-cols-2 md:grid-cols-4 grid-flow-row-dense gap-3 sm:gap-4">
-        <TileVideo className="col-span-1 row-span-2" />
-        <TileTelegram className="col-span-1 row-span-1 aspect-square" />
-        <TileInstagram className="col-span-1 row-span-1 aspect-square " />
-        <TileTikTok className="col-span-1 row-span-1 aspect-square" />
-        <TileNewsletter className="col-span-2 md:col-span-2 row-span-1" />
-        <TileDaria className="col-span-1 row-span-1 aspect-square" />
+      <div className="grid gap-3 sm:gap-4">
+        <div className="grid gap-3 sm:gap-4 md:grid-cols-4">
+          <H as="h2" className="col-span-full">
+            Stay in touch,
+            <br /> give us a follow
+          </H>
+          <Tile className="bg-orange-200 p-3 sm:p-4 col-span-full md:col-span-3">
+            <p>You need content? We have content content content</p>
+            <p>
+              Check out our <A href="https://telegram.org/">Telegram</A>,{' '}
+              <A href="https://www.instagram.com/watch.with.daria/">
+                Instagram
+              </A>{' '}
+              and <A href="https://www.tiktok.com/@watch.with.ddaria">TikTok</A>{' '}
+              to stay up to date.
+            </p>
+            <p>
+              You need more? We got you covered: Subscribe to our{' '}
+              <A to="/">Newsletter</A> for the latest tasty treats!
+            </p>
+          </Tile>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 grid-flow-row-dense gap-3 sm:gap-4">
+          <TileVideo className="col-span-1 row-span-2" />
+          <TileTelegram className="col-span-1 row-span-1 aspect-square" />
+          <TileInstagram className="col-span-1 row-span-1 aspect-square " />
+          <TileTikTok className="col-span-1 row-span-1 aspect-square" />
+          <TileNewsletter className="col-span-2 md:col-span-2 row-span-1" />
+          <TileDaria className="col-span-1 row-span-1 aspect-square" />
+        </div>
       </div>
     </Section>
   );
--- src/components/tiles/tile-newsletter.tsx
+++ src/components/tiles/tile-newsletter.tsx
@@ -19,11 +19,11 @@ export const TileNewsletter = ({ className }: ComponentProps<'div'>) => {
           order.
         </p>
         <form>
-          <div data-style="clean" className="flex items-end mb-3">
+          <div data-style="clean" className="flex items-end">
             <div
               data-element="fields"
               data-stacked="false"
-              className="flex flex-col md:flex-row gap-3 sm:gap-4 w-full max-w-md mb-3 seva-fields formkit-fields"
+              className="flex flex-col md:flex-row gap-3 sm:gap-4 w-full max-w-md seva-fields formkit-fields"
             >
               <div className="relative w-full formkit-field">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
