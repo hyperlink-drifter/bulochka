7783fb8966974f1fa12f606e31875bc635753c39
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-22 13:42:43 +0200

    feat: update waku @0.23.1

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -4,17 +4,17 @@
 import type { PathsForPages, GetConfigResponse } from 'waku/router';
 
 // prettier-ignore
-import type { getConfig as Root_getConfig } from './pages/_root';
+import type { getConfig as File_Root_getConfig } from './pages/_root';
 // prettier-ignore
-import type { getConfig as About_getConfig } from './pages/about';
+import type { getConfig as File_About_getConfig } from './pages/about';
 // prettier-ignore
-import type { getConfig as Index_getConfig } from './pages/index';
+import type { getConfig as File_Index_getConfig } from './pages/index';
 
 // prettier-ignore
 type Page =
-| ({ path: '/_root' } & GetConfigResponse<typeof Root_getConfig>)
-| ({ path: '/about' } & GetConfigResponse<typeof About_getConfig>)
-| ({ path: '/' } & GetConfigResponse<typeof Index_getConfig>);
+| ({ path: '/_root' } & GetConfigResponse<typeof File_Root_getConfig>)
+| ({ path: '/about' } & GetConfigResponse<typeof File_About_getConfig>)
+| ({ path: '/' } & GetConfigResponse<typeof File_Index_getConfig>);
 
 // prettier-ignore
 declare module 'waku/router' {
