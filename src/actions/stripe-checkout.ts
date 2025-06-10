7a60c276a7ef0bf4ab4df21e17e2735322ef93a8
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-06-10 17:47:40 +0200

    feat: stripe checkout action

--- src/actions/stripe-checkout.ts
+++ src/actions/stripe-checkout.ts
@@ -7,31 +7,33 @@ export const stripe = {
   createCheckout: defineAction({
     accept: "form",
     input: z.object({
-      slug: z.string(),
       id: z.string(),
     }),
     handler: async (input) => {
       const stripe = new Stripe(STRIPE_SECRET_KEY);
-      try {
-        const session = await stripe.checkout.sessions.create({
-          line_items: [
-            {
-              // Provide the exact Price ID (for example, price_1234) of the product you want to sell
-              price: input.id,
-              quantity: 1,
-            },
-          ],
-          mode: "payment",
-          success_url: "https://waku.gg/",
-          cancel_url: "https://docs.stripe.com/payments/checkout",
-        });
 
-        return {
-          url: session.url,
-        };
-      } catch (error: unknown) {
-        return { error: "" };
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price: input.id,
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: "https://waku.gg/",
+        cancel_url: "https://docs.stripe.com/payments/checkout",
+      });
+
+      if (!session.url) {
+        throw new Error("Failed to create Stripe checkout session", {
+          cause: session,
+        });
       }
+
+      return {
+        url: session.url,
+      };
     },
   }),
 };
